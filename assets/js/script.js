872f11b7210e8141addc4b07cb68fff139c4fc18
simon <simonbreen567@gmail.com>
2023-01-16 20:39:16 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ document.getElementById("answer-box").value = "";
 document.getElementById("answer-box").focus();
 // The above relates to "Setting the focus" were the cursor starts in the box that the user has to input
 
-  // reates two random numbers between 1 and 25
+  // creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
 
@@ -48,6 +48,8 @@ document.getElementById("answer-box").focus();
     displayMultiplyQuestion(num1,num2);
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type; ${gameType}. Aborting!`;
@@ -91,7 +93,9 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
-  }else {
+  } else if (operator === "/") {
+    return [operand1 / operand2, "division"];
+  } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
   }
@@ -131,4 +135,10 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1 * operand2;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "/";
 }
